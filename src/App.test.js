66b67def03c414b9264240c66639e9363f262f17
Lynda Santiago <you@example.com>
2020-08-06 16:52:39 -0400

    created test for form

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,18 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("form is filled out and can submit", () => {
+  render(<ContactForm />);
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  const emailInput = screen.getByLabelText(/email/i);
+  const messageInput = screen.getByLabelText(/message/i);
+  
+  screen.debug()
 });
